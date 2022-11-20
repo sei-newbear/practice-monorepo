8a0e1409f6d1761c4bdc7faf9bb38feab93512e8
sei.shinkuma <shinkuma.sei@gmail.com>
2022-11-20 23:16:51 +0900

    front change SSR to SPA and API do CORS

--- front/app/src/routes/index.svelte
+++ front/app/src/routes/index.svelte
@@ -1,26 +1,39 @@
 <script lang="ts">
-    type Task = {
-        id: number,
-        title: string
-    }
-    type Tasks = {
-        total: number,
-        tasks: Task[]
-    }
+	import { onMount } from 'svelte';
 
-    export let tasks: Tasks = {total: 2, tasks: [{id: 1, title: "test1"}]}
+	type Task = {
+		id: number;
+		title: string;
+	};
+	type Tasks = {
+		total: number;
+		tasks: Task[];
+	};
+
+	export let tasks: Tasks = {total:0, tasks: []};
+
+	onMount(async () => {
+		const response = fetch(`http://localhost:8089/tasks`, {
+			method: 'GET',
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		tasks = await (await response).json();
+	});
 </script>
 
 <svelte:head>
-    <title>Tasks</title>
+	<title>Tasks</title>
 </svelte:head>
 
 <div class="tasks">
-    <h1>Tasks</h1>
+	<h1>Tasks</h1>
 
-    {#each tasks.tasks as task}
-        <div class="task">
-            <span class="id">{task.id}</span>: <span class="title">{task.title}</span>
-        </div>
-    {/each}
+	{#each tasks.tasks as task}
+		<div class="task">
+			<span class="id">{task.id}</span>: <span class="title">{task.title}</span>
+		</div>
+	{/each}
 </div>
