29db1dc709431890a1a468d8da1f77ca63b3cbb5
sei.shinkuma <shinkuma.sei@gmail.com>
2022-11-21 00:06:23 +0900

    change task api port

--- env/task-api/run_api_mock.sh
+++ env/task-api/run_api_mock.sh
@@ -3,4 +3,4 @@
 # docker run -it --rm -p 8089:8080 --name wiremock -v $PWD:/home/wiremock wiremock/wiremock
 
 docker build . -t apimock
-docker run -p 8089:8080 -it --rm apimock
+docker run -p 8089:8089 -it --rm apimock
